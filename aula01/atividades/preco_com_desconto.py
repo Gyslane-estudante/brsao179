78ae17e1155bc94ee3bbbdc93c088b8fde281ad5
Gyslane Anunciação de Oliveira <gyslane.oliveira@websis.com.br>
2025-05-15 20:32:15 -0300

    Remove todos os arquivos e pastas antigos

--- aula01/atividades/preco_com_desconto.py
+++
@@ -1,6 +0,0 @@
-preco = 1000
-desconto = 15
-
-valor_final = preco - (preco * desconto / 100)
-valor_format = f'{valor_final:.2f}'
-print(f"\nO preço do celular de Maria com {desconto}% de desconto é R$ {valor_format}")
