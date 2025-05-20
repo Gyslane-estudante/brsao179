698440447f4084c5c52b1779896ecd020daae82c
Gyslane Anunciação de Oliveira <gyslane.oliveira@websis.com.br>
2025-05-20 19:55:26 -0300

    Conteúdo da aula 01

---
+++ aula01/teoria/identacao.py
@@ -0,0 +1,9 @@
+if True:
+    print("Primeiro nível")
+    if True:
+        print("Segundo nível")
+        print("Ainda no segundo nível")
+    print("Voltei para o primeiro nível")
+print("Fora de tudo")
+
+#O código acima é um exemplo de indentação em Python
---
+++ aula01/teoria/variaveis.py
@@ -0,0 +1,12 @@
+nome = "Gyslane"
+nome = "Gyslane"
+nome = 2025
+sobrenome_gyslane = "Oliveira"
+Nome = "Gyslane2"
+
+idade = 33
+altura = 1.69
+
+print(nome)
+print(type(idade))
+print(type(altura))
